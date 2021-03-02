ff3b6ea8bca28ac124d4afeba887c5b73e2f3abc
Mental <73140396+MaximKing1@users.noreply.github.com>
2021-03-02 18:03:34 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ class Poster extend EventEmitter {
     super(options);
     this.client = client;
     this.token = token;
+    this.api = ("https://api.infinitybots.xyz/");
  }
 
 async autoPost(options={}) {
  if(options.timerLoop =< "300000") return console.log("[IBL] Your Loop Can't Be More Than Every 5 Minutes!");
  it(typeof options.timerLoop !== "number") throw new Error("[IBL] TimerLoop Needs To Be In MS and Numbers!");
 
+ // Set Options
+ options.botID = this.botID;
+ options.timerLoop = this.loop;
  }
 }
 module.exports = Poster;
